f40b69ada4e0da01ed95322a993a0aa568ec407c
hellokenday <hello@ken-day.co.uk>
2014-02-20 16:38:30 +0000

    finished settings views

--- src/www/javascripts/application.js
+++ src/www/javascripts/application.js
@@ -1,4 +1,3 @@
-
 // steroids.view.navigationBar.show("Hello World");
 
 function showCamera() {
@@ -6,32 +5,35 @@ function showCamera() {
   steroids.layers.push(webView);
 }
 
-steroids.view.navigationBar.show("Layer Stack");
-
 
 function showFirebase() {
   var webView = new steroids.views.WebView("views/firebase/index.html");
   steroids.layers.push(webView);
 }
 
-steroids.view.navigationBar.show("Layer Stack");
 
 function showDrawer() {
   var webView = new steroids.views.WebView("views/drawer/index.html");
   steroids.layers.push(webView);
 }
 
-steroids.view.navigationBar.show("Layer Stack");
-
 function showGPS() {
   var webView = new steroids.views.WebView("views/gps/index.html");
   steroids.layers.push(webView);
 }
 
-steroids.view.navigationBar.show("Layer Stack");
-
 function showSettings() {
   var webView = new steroids.views.WebView("views/settings/index.html");
+  steroids.layers.push(webView);
+}
+
+function showPrivacy() {
+  var webView = new steroids.views.WebView("views/privacy/index.html");
+  steroids.layers.push(webView);
+}
+
+function showFeedback() {
+  var webView = new steroids.views.WebView("views/feedback/index.html");
   steroids.layers.push(webView);
 }
 
