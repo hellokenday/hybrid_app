b2d88573a3fd091241a7f9b0e267a72689fa1eea
hellokenday <hello@ken-day.co.uk>
2014-02-22 22:14:11 +0000

    geolocation and basic map
    test for cordova geolocation and google maps api

--- src/app/controllers/gps.js
+++ src/app/controllers/gps.js
@@ -46,3 +46,51 @@ function startTracking_stop(){
   $("#startTracking_status").html("Stopped tracking workout: <strong>" + track_id + "</strong>");
  
 }
+
+// --- Current gps position --- //
+
+// Wait for device API libraries to load
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+var watchID = null;
+
+// device APIs are available
+//
+function onDeviceReady() {
+    // Throw an error if no update is received every 30 seconds
+    var options = { timeout: 30000 };
+    watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+}
+
+// onSuccess Geolocation
+//
+function onSuccess() {
+    var win = function(position) {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        var myLatlng = new google.maps.LatLng(lat, long);
+
+        var myOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.HYBRID
+        };
+        map_element = document.getElementById("map_canvas");
+        map = new google.maps.Map(map_element, myOptions);
+    };
+
+    var fail = function(e) {
+        $.mobile.hidePageLoadingMsg();
+        alert('Can\'t retrieve position.\nError: ' + e);
+    };
+
+    watchID = navigator.geolocation.getCurrentPosition(win, fail);
+} 
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
