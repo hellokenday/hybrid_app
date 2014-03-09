00564eb3e14f017dfb7e96633d1df7d1fac48bf3
hellokenday <hello@ken-day.co.uk>
2014-03-09 20:01:21 +0000

    Show basic map on tab 2

--- switchback/app/controllers/gps.js
+++ switchback/app/controllers/gps.js
@@ -1,4 +1,4 @@
-	steroids.navigationBar.show("GPS");
+steroids.navigationBar.show("GPS");
 // --- Map Panel --- //
 
 // Wait for device API libraries to load
@@ -22,14 +22,21 @@ var tab2Inited = false;
 var tab3Inited = false;
 
 function initCurrentLocationMap () {
+
+    console.log('initCurrentLocationMap');
+
+    // give a random centrepoint for now (brighton pier)
+    var centre = new google.maps.LatLng(50.815514, -0.137089);
+
     var mapOptions = {
-        center: latlng,
+        center: centre,
         zoom: 17,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         disableDefaultUI: true
     };
 
     currentLocationMap = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    //watchID = navigator.geolocation.watchPosition(onGetCurrentLocationSuccess, onGetCurrentLocationError, options);
 
     tab2Inited = true;
 }
@@ -55,13 +62,15 @@ function initSegmented (argument) {
 
 function getCurrentLocation() {
     
-    console.log('_getCurrentLocation');
+    console.log('getCurrentLocation');
     
-    if(!tab2Inited) _initCurrentLocationMap();
-  
-    // Throw an error if no update is received every 30 seconds
-    watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+    if(tab2Inited === false) initCurrentLocationMap();
+    else watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
 }
+
+/**
+ * Event handling
+ */
 
 function onDeviceReady () {
     initSegmented();
@@ -75,7 +84,7 @@ function onTabClicked(tabIndex) {
             console.log('1');
             break;
         case 1:
-            console.log('2');
+            getCurrentLocation();
             break;
         case 2:
             console.log('3');
@@ -86,24 +95,20 @@ function onTabClicked(tabIndex) {
 }
 
 // onSuccess Geolocation
-function onSuccess() {
+function onGetCurrentLocationSuccess() {
 
-    // function snippet from: http://zsprawl.com/iOS/2012/03/using-phonegap-with-google-maps/
-    var win = function(position) {
+    var success = function(position) {
 
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
-        var latlng = new google.maps.LatLng(lat, long);
-
+        var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
         var marker = new google.maps.Marker({
             position: latlng,
-            map: map,
-            title:"You are here"
+            map: currentLocationMap,
+            title:"Current location"
         });
     };
-    var fail = function(e) { $.noop(); };
+    var failure = function(e) { $.noop(); };
 
-    watchID = navigator.geolocation.getCurrentPosition(win, fail);
+    watchID = navigator.geolocation.getCurrentPosition(success, failure);
 }
-function onError(e) { console.log('code: '    + e.code    + '\n' + 'message: ' + e.message + '\n'); }
+function onGetCurrentLocationError(e) { console.log('onGetCurrentLocationError: '    + e.code    + ': ' + 'message: ' + e.message); }
 
