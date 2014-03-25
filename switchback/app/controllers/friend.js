b7186ebffc77d38d911e3a2973907b1a74c7f99b
hellokenday <hello@ken-day.co.uk>
2014-03-25 19:14:55 +0000

    friend screen navigation and 'block' pop up message

--- switchback/app/controllers/friend.js
+++ switchback/app/controllers/friend.js
@@ -6,56 +6,73 @@ var editProfileView = new steroids.views.WebView("views/edit_profile/index.html"
 editProfileView.preload();
 
 // Edit and Save buttons
-//var editButton = new steroids.buttons.NavigationBarButton();
-//editButton.title = "Edit";
-//
-//var logOutButton = new steroids.buttons.NavigationBarButton();
-//logOutButton.title = "Log Out";
+var flagButton = new steroids.buttons.NavigationBarButton();
+flagButton.imagePath = "/icons/flag@2x.png";
+
+var backButton = new steroids.buttons.NavigationBarButton();
+backButton.imagePath = "/icons/back_btn@2x.png";
 
 function onDeviceReady() {
-    initButtons();
+    initNavBar();
     disableScrolling();
     initChuiSheet();
     initSheetButtons();
-//    initNavBar();
 } 
 
-//function initNavBar() {
-//    steroids.view.navigationBar.show();
-//    steroids.view.navigationBar.update({
-//        title: "Profile",
-//        overrideBackButton: false,
-//        buttons: {
-//            left: [logOutButton],
-//            right: [editButton],
-//        }
-//    });
-//}
+function initNavBar() {
+    steroids.view.navigationBar.show();
+    steroids.view.navigationBar.update({
+        title: "Friend",
+        overrideBackButton: true,
+        buttons: {
+            left: [backButton],
+            right: [flagButton]
+        }
+    });
+}
 
 function initChuiSheet() {
 
     $.UISheet();
     $('.sheet').find('section').append("<ul class='list'></li>");
-    $('.sheet .list').append("<li><a class='button' href='javascript:void(null)'>Log Out</a></li><li><a class='button' href='javascript:void(null)'>Cancel</a></li>");
-}
-
-function initButtons() {
-
-    $('.back_btn').on('singletap', backToPeople);
-    $('.settings_btn').on('singletap', showSettingsControls);
+    $('.sheet .list').append("<li><a class='button mute_btn' href='javascript:void(null)'>Mute friend_Name</a></li><li><a class='button block_btn' href='javascript:void(null)'>Block friend_Name</a></li><li><a class='button cancel' href='javascript:void(null)'>Cancel</a></li>");
 }
 
 function initSheetButtons() {
-    $('.sheet .list').on('singletap', '.button', closeSettingsControls);
+    
+    $('.mute_btn').on('singletap', closeShowMuteOptions);
+    $('.block_btn').on('singletap', closeShowBlockView);
+    $('.cancel').on('singletap', closeFlagControls);
 }
 
-function showSettingsControls() {
+function closeFlagControls() {
     
-    $.UIShowSheet();
+    $.UIHideSheet();
 }
 
-function closeSettingsControls() {
-    
+function closeShowMuteOptions() {
+    $.UIHideSheet();
+    showMuteOptions();
+}
+
+function showMuteOptions() {
+    $.UIPopup({
+        id: "warning",
+        title: 'Mute Friend_Name', 
+        message: 'You will stop getting notifications from friend_name. You can still see posts in your stream or on their profile.', 
+        cancelButton: 'Cancel', 
+        continueButton: 'Mute', 
+        callback: function() {
+         // var popupMessageTarget = document.querySelector('#popupMessageTarget');
+         // popupMessageTarget.textContent = 'Thanks for staying with us a bit longer.';
+            popupMessageTarget.className = "";
+            popupMessageTarget.className = "animatePopupMessage";
+        }
+    });
+}
+
+function closeShowBlockView() {
+    alert('closeShowBlockView');
     $.UIHideSheet();
 }
 
@@ -71,3 +88,13 @@ function disableScrolling() {
         event.preventDefault();
     });
 }
+
+backButton.onTap = function() {
+
+    steroids.layers.pop(); 
+};
+
+flagButton.onTap = function() {
+
+    $.UIShowSheet(); 
+};
